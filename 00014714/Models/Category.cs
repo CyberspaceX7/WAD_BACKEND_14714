562f92dfb6782ab725770fde49765736ecbf3774
CyberspaceX7 <kh.jurakulov@gmail.com>
2024-03-18 11:50:01 +0500

    Created Survey and Category classes.

---
+++ 00014714/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _00014714.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        private string _name;
+        [Required(ErrorMessage = "Name is required")]
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+
+                _name = value;
+            }
+        }
+    }
+}
---
+++ 00014714/Models/Survey.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace _00014714.Models
+{
+    public class Survey
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Question one is required")]
+        public string? Question1 { get; set; }
+
+        [Required(ErrorMessage = "Question two is required")]
+        public string? Question2 { get; set; }
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+    }
+}
